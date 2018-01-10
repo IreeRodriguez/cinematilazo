e54e2c88870e21515f0299846390f323511fd78e
IreeRodriguez <ire208@gmail.com>
2018-01-10 15:06:30 -0300

    funcionalidad de barra de busqueda

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,14 +14,61 @@ function search(input) {
                 console.log(v);
                 $.each(v, function(j,value){
                     console.log(value.Title);
+
+
+                    var movieModal = 'http://www.omdbapi.com/?apikey=3a181f1c&t=' + value.Title ;
+                    getModal(movieModal);
+
+                    //Data para obtener el Modal
+                    function getModal(movieModal) {
+                        $.getJSON(movieModal, function(data){
+                          console.log(data);
+                          $.each(data, function(i,val) {
+                                //console.log(data.Released);
+                             $('#titles').append(
+
+                           '<div  id="myModal' + j + '" role="dialog" class="modal fade"  tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">' +
+                            '<div class="modal-dialog" role="document">' +
+                            '<div class="modal-content">' +
+                            '<div class="modal-header">' +
+                            '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>' +
+                            '<h4 class="modal-title text-uppercase ">' + data.Title + '</h4>' +
+                            '</div>' +
+                            '<div class="modal-body">' +
+                            '<p>' + data.Director + '</p>' +
+                            '<img class="img-fluid " src=" '+data.Poster +'" alt=""> ' +
+                             '<ul class="list-inline">' +
+                            ' <li>Año: ' + data.Year + '</li>' +
+                            ' <li>Premios: ' + data.Awards + '</li>' +
+                            ' <li>Category: ' + data.Genre + '</li>' +
+                            ' </ul>' +
+                            '<p>' + data.Plot+ '</p>' +
+                            '</div>' +
+                            '<div class="modal-footer">' +
+                           ' <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>' +
+                            '<button type="button" class="btn btn-primary">Save changes</button>' +
+                            '</div>' +
+                            '</div><!-- /.modal-content -->' +
+                            '</div><!-- /.modal-dialog -->' +
+                            '</div><!-- /.modal -->'
+
+                                        )
+                           })
+                        })
+                    };
+
+
                     $('#titles').append(
-                        "<a class='nombre" + j + "' href='#'>" + value.Title + "</a>"
 
-
+                  ' <div class="col-md-3">' +
+                  ' <h4><a type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal' + j + '" >'+ value.Title + '</a></h4> ' +
+                  '<div class="bestMovie">' +
+                  '<img src="' + value.Poster + '">' +
+                  '</div></div>'
                     );
 
-                })
-            }
+                });
+            };
         });
 
     });
@@ -31,16 +78,17 @@ $('#search').click(function(){
     var input = $('#userTitle').val();
 
     var movieSearch = 'http://www.omdbapi.com/?apikey=3a181f1c&s=' + input;
-    console.log(movieSearch);
+    //console.log(movieSearch);
     search(movieSearch);
-
+    $('#userTitle').val('');
+    $('#titles').empty();
+    $('#recommended').hide();
+    $('#slide').hide();
+    $('#profileUser').hide();
+    $('#splash').hide();
 
 });
 
-$('.nombre0').click(function(){
-    console.log('hola');
-
-});
 //funcion carrusel
 $(function() {
     $('.tooltip-carousel').popover();
@@ -66,3 +114,10 @@ $(function() {
     });
   });
 //funcion carrusel fin
+/*   <section>
+         <input type="text" class="form-control" id="userTitle" placeholder="Text input">
+         <input class="btn btn-default" type="submit" id="search" value="Submit">
+
+
+        </section>
+        <div id="titles"></div> */
