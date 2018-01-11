cbe6bc1d057a3831d30aff873c6a9271fd82f5c3
IreeRodriguez <ire208@gmail.com>
2018-01-11 10:19:55 -0300

    funcionalidad con firebase: login, y chat

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,21 @@
 $( document ).ready(function() {
 
-  $('#recommended').hide();
-  $('#slide').hide();
-  $('#profileUser').hide();
-  $('.userHeader').hide();
-  $('footer').hide();
+  // $('#recommended').hide();
+  // $('#slide').hide();
+  // $('#profileUser').hide();
+  // // $('.userHeader').hide();
+  // $('footer').hide();
 
 
    setTimeout(function() {
       $('#splash').fadeOut(800);
    }, 2500);
 
-    
+
 });
 
 function ingreso(){
-  
+
   $('.joinHeader').hide();
   $('.registry').hide();
   $('.userHeader').show();
@@ -24,11 +24,11 @@ function ingreso(){
   $('#profileUser').show();
   $('#userHeader').show();
   $('footer').show();
-   
+
 
 };
 $('.smallLogo').click(function(){
-  
+
   ingreso();
 })
 
@@ -141,11 +141,380 @@ $(function() {
       $(this).popover('hide');
     });
   });
-//funcion carrusel fin
-/*   <section>
-         <input type="text" class="form-control" id="userTitle" placeholder="Text input">
-         <input class="btn btn-default" type="submit" id="search" value="Submit">
+
+  function socialNet() {
+    this.checkSetup();
+
+    // DOM elements//
+    this.messageList = document.getElementById('messages');
+    this.messageForm = document.getElementById('message-form');
+    this.messageInput = document.getElementById('message');
+    this.submitButton = document.getElementById('submit');
+    this.submitImageButton = document.getElementById('submitImage');
+    this.imageForm = document.getElementById('image-form');
+    this.mediaCapture = document.getElementById('mediaCapture');
+    this.userPic = document.getElementById('user-pic');
+    this.modalPic = document.getElementById('modal-pic');
+    this.userName = document.getElementById('user-name');
+    this.modalName = document.getElementById('modal-name');
+    this.signInButton = document.getElementById('sign-in');
+    this.signOutButton = document.getElementById('sign-out');
+    this.feed = document.getElementById('feed');
+    // this.about = document.getElementById('about');
+    // this.info = document.getElementById('inf');
+    this.instrument = document.getElementById('instrumento');
+    this.genero = document.getElementById('genero');
+    this.age = document.getElementById('age');
+    this.exp = document.getElementById('exp');
+    this.lvlExp = document.getElementById('lvlExp');
+    this.genre = document.getElementById('genre');
+    this.save = document.getElementById('save');
+
+    this.userUid = 0;
+
+    // firebase.auth().onAuthStateChanged((user) => {
+    //   if (user) {
+    //     this.userUid = user.uid;
+    //   }
+    // });
 
 
-        </section>
-        <div id="titles"></div> */
+
+
+
+
+    // Guardar mensajes del input al presionar submit//
+    this.messageForm.addEventListener('submit', this.saveMessage.bind(this));
+    this.signOutButton.addEventListener('click', this.signOut.bind(this));
+    this.signInButton.addEventListener('click', this.signIn.bind(this));
+    this.save.addEventListener('click', this.saveProfile.bind(this));
+
+    this.userName.addEventListener('click', this.loadProfile.bind(this));
+
+
+    // subir una imagen en el feed//
+    this.submitImageButton.addEventListener('click', function(e) {
+      e.preventDefault();
+      this.mediaCapture.click();
+    }.bind(this));
+    this.mediaCapture.addEventListener('change', this.saveImageMessage.bind(this));
+
+    this.initFirebase();
+  }
+
+  // inicializar firebase y los productos a usar//
+  socialNet.prototype.initFirebase = function() {
+      // productos de firebase//
+      this.auth = firebase.auth();
+      this.database = firebase.database();
+      this.storage = firebase.storage();
+      // escuchar si hay cambios en el estado de usuario.//
+      this.auth.onAuthStateChanged(this.onAuthStateChanged.bind(this));
+  };
+
+  // cargar mensajes desde la base de datos//
+  socialNet.prototype.loadMessages = function() {
+      // /messages/ database path.
+      this.messagesRef = this.database.ref('messages');
+      // Make sure we remove all previous listeners.
+      this.messagesRef.off();
+
+
+      // cargar solo los ultimos 12 y escuchar si hay nuevos//
+      var setMessage = function(data) {
+        var val = data.val();
+        //console.log(val.name);
+        this.displayMessage(data.key, val.name, val.text, val.photoUrl, val.imageUrl);
+      }.bind(this);
+      this.messagesRef.limitToLast(12).on('child_added', setMessage);
+      this.messagesRef.limitToLast(12).on('child_changed', setMessage);
+  };
+
+
+  //Guardar mensaje nuevo en firebase//
+  socialNet.prototype.saveMessage = function(e) {
+    e.preventDefault();
+    // chequear por mensaje nuevo y usuario logeado//
+    if (this.messageInput.value && this.checkSignedInWithMessage()) {
+
+        var currentUser = this.auth.currentUser;
+       // nueva entrada en la database//
+       this.messagesRef.push({
+         name: currentUser.displayName,
+         text: this.messageInput.value,
+         photoUrl: currentUser.photoURL || '/images/profile_placeholder.png'
+       }).then(function() {
+         // borrar input para que quede vacio//
+         socialNet.resetMaterialTextfield(this.messageInput);
+         this.toggleButton();
+       }.bind(this)).catch(function(error) {
+         // console.log('5');
+       });
+    }
+  };
+  //cargar perfil de usuario
+  socialNet.prototype.loadProfile = function(){
+      var profRef = this.database.ref('profile');
+      var profileBody = document.getElementById('profileBody');
+      var modalInfo = document.getElementById('profileInfo');
+      var modalSubmit = document.getElementById('modal-submit');
+      var profileClose = document.getElementById('profileClose');
+      var profileGenero = document.getElementById('profileGenero');
+      var profileInst = document.getElementById('profileInst');
+      var profileAge = document.getElementById('profileAge');
+      var profileExp = document.getElementById('profileExp');
+      var profileLvlExp = document.getElementById('profileLvlExp');
+      var profileGenre = document.getElementById('profileGenre');
+
+          var searchId = function(snapshot) {
+          var obj = snapshot.val();
+
+          for (key in obj) {
+              if (obj.hasOwnProperty(key)) {
+
+                  if (this.checkSignedInWithMessage()) {
+
+                      if (this.userUid === obj[key].userid) {
+
+                          modalInfo.classList.add('hide');
+                          modalSubmit.classList.add('hide');
+                          profileBody.classList.remove('hide');
+                          profileClose.classList.remove('hide');
+                          profileGenero.innerHTML = obj[key].genero;
+                          profileAge.innerHTML = obj[key].age;
+                          profileInst.innerHTML = obj[key].instrument;
+                          profileExp.innerHTML = obj[key].exp;
+                          profileLvlExp.innerHTML = obj[key].lvlExp;
+                          profileGenre.innerHTML = obj[key].genre;
+                          console.log("Found match!!");
+                          return true;
+                      } else {
+                          console.log('boo');
+                          profileBody.classList.add('hide');
+                          profileClose.classList.add('hide');
+                          modalSubmit.classList.remove('hide');
+                          modalInfo.classList.remove('hide');
+                      }
+                      }
+
+              }
+          }
+      }.bind(this);
+
+      profRef.on("value", searchId);
+
+  };
+  //guardar profile en firebase
+  socialNet.prototype.saveProfile = function(e) {
+      e.preventDefault();
+      console.log('funcion saveprofile activated');
+
+      this.profileRef = this.database.ref('profile');
+
+      this.profileRef.off();
+
+      var currentUser = this.auth.currentUser;
+      console.log(this.instrument.value);
+        console.log(this.userUid);
+
+      this.profileRef.push({
+          name: currentUser.displayName,
+          genero: this.genero.value,
+          age: this.age.value,
+          instrument: this.instrument.value,
+          exp: this.exp.value,
+          lvlExp: this.lvlExp.value,
+          genre: this.genre.value,
+          userid : this.userUid,
+      });
+  };
+
+
+
+  // Sets URL de la imagen uplodeada con el url de la imagen que ahora esta guardada en firebase storage//
+  socialNet.prototype.setImageUrl = function(imageUri, imgElement) {
+      // si la imagen esta en firebase su url comenzara con gs:// y rescatamos este url de la base datos.
+      if (imageUri.startsWith('gs://')) {
+        imgElement.src = socialNet.LOADING_IMAGE_URL; // imagen de espera mientras carga//
+        this.storage.refFromURL(imageUri).getMetadata().then(function(metadata) {
+          imgElement.src = metadata.downloadURLs[0];
+        });
+      } else {
+        imgElement.src = imageUri;
+      }
+  };
+
+  // Guardar el nuevo mensaje con la imagen
+
+  socialNet.prototype.saveImageMessage = function(event) {
+    event.preventDefault();
+    var file = event.target.files[0];
+
+    // borrar la seleccion.
+    this.imageForm.reset();
+
+    // chequear si el archivo enviado es una imagen//
+    if (!file.type.match('image.*')) {
+        alert('Solo puedes compartir imagenes');
+      return;
+    }
+    // si el usuario esta logeado//
+    if (this.checkSignedInWithMessage()) {
+
+        // se guarda la imagen en el storage de firebase, de nuevo imagen de cargando mientras se guarda la imagen
+           var currentUser = this.auth.currentUser;
+           this.messagesRef.push({
+             name: currentUser.displayName,
+             imageUrl: socialNet.LOADING_IMAGE_URL,
+             photoUrl: currentUser.photoURL || '/images/profile_placeholder.png'
+           }).then(function(data) {
+
+             var filePath = currentUser.uid + '/' + data.key + '/' + file.name;
+             return this.storage.ref(filePath).put(file).then(function(snapshot) {
+
+               var fullPath = snapshot.metadata.fullPath;
+               return data.update({imageUrl: this.storage.ref(fullPath).toString()});
+             }.bind(this));
+           }.bind(this)).catch(function(error) {
+             console.error('There was an error uploading a file to Cloud Storage:', error);
+           });
+    }
+  };
+
+  // loguarse en la pagina
+  socialNet.prototype.signIn = function() {
+      // se usa cuenta de google para acceder
+        var provider = new firebase.auth.GoogleAuthProvider();
+        this.auth.signInWithPopup(provider);
+  };
+
+  // Signs-out de la pagina//
+  socialNet.prototype.signOut = function() {
+      // Sign out de Firebase.//
+     this.auth.signOut();
+
+  };
+
+  // activa eventos cunado hay cambios en el estado de usuario, si esta logeado o no
+  socialNet.prototype.onAuthStateChanged = function(user) {
+    if (user) { // si el usuario esta signed in//
+      // se toma el su foto de perfil de google y su nombre//
+      var profilePicUrl = user.photoURL;
+      var userName = user.displayName;
+      this.userUid = user.uid;
+
+      // se coloca nombre y foto en la pagina//
+      this.userPic.style.backgroundImage = 'url(' + profilePicUrl + ')';
+      this.modalPic.style.backgroundImage = 'url(' + profilePicUrl + ')';
+      this.userName.textContent = userName;
+      this.modalName.textContent = userName;
+
+      // se muestra el boton de sign out  y se muestra info del usuario y feed y se oculta el boton de sing in
+      this.userName.classList.remove('hide');
+      this.userPic.classList.remove('hide');
+      this.feed.classList.remove('hide');
+      // this.about.classList.add('hide');
+      // this.info.classList.add('hide');
+
+      this.signOutButton.classList.remove('hide');
+      this.signInButton.classList.add('hide');
+
+      // se cargan los mensajes de la base de datos//
+      this.loadMessages();
+      this.loadProfile();
+
+  } else { // si el usuario esta signed out//
+      // se oculta boton de sign out e informacion del usuario y feed, se muentra el boton de sign in
+      this.userName.classList.add('hide');
+      this.userPic.classList.add('hide');
+      this.feed.classList.add('hide');
+      // this.about.classList.remove('hide');
+      // this.info.classList.remove('hide');
+
+      this.signOutButton.classList.add('hide');
+      this.signInButton.classList.remove('hide');
+    }
+  };
+
+  // retorna true si el usuario esta signed in
+  socialNet.prototype.checkSignedInWithMessage = function() {
+
+      if (this.auth.currentUser) {
+       return true;
+      } else {
+       return false;
+      }
+  };
+
+
+  // limpia o borra input de mensajes
+  socialNet.resetMaterialTextfield = function(element) {
+    element.value = '';
+    element.parentNode.MaterialTextfield.boundUpdateClassesHandler();
+  };
+
+  // plantilla para los mensajes anadidos//
+  socialNet.MESSAGE_TEMPLATE =
+      '<div class="message-container">' +
+        '<div class="spacing"><div class="pic"></div></div>' +
+        '<div class="message"></div>' +
+        '<div class="name"></div>' +
+      '</div>';
+
+  //imagen de espera
+  socialNet.LOADING_IMAGE_URL = 'https://www.google.com/images/spin-32.gif';
+
+  // mostrar mensajes en la pagina
+  socialNet.prototype.displayMessage = function(key, name, text, picUrl, imageUri) {
+    var div = document.getElementById(key);
+
+    if (!div) {
+      var container = document.createElement('div');
+      container.innerHTML = socialNet.MESSAGE_TEMPLATE;
+      div = container.firstChild;
+      div.setAttribute('id', key);
+      this.messageList.appendChild(div);
+    }
+    if (picUrl) {
+      div.querySelector('.pic').style.backgroundImage = 'url(' + picUrl + ')';
+    }
+    div.querySelector('.name').textContent = name;
+    var messageElement = div.querySelector('.message');
+    if (text) { //si el mensaje es texto
+      messageElement.textContent = text;
+      // reemplazar cambio de linea con <br>//
+      messageElement.innerHTML = messageElement.innerHTML.replace(/\n/g, '<br>');
+  } else if (imageUri) { // si es una imagen//
+      var image = document.createElement('img');
+      image.addEventListener('load', function() {
+        this.messageList.scrollTop = this.messageList.scrollHeight;
+      }.bind(this));
+      this.setImageUrl(imageUri, image);
+      messageElement.innerHTML = '';
+      messageElement.appendChild(image);
+    }
+  };
+
+  //desabilitar y habilitar boton de enviar
+  socialNet.prototype.toggleButton = function() {
+    if (this.messageInput.value) {
+      this.submitButton.classList.remove('disabled');
+    } else {
+      this.submitButton.classList.add('disabled');
+    }
+  };
+
+  // chequear si el sdk de firebase esta correcto
+  socialNet.prototype.checkSetup = function() {
+    if (!window.firebase || !(firebase.app instanceof Function) || !firebase.app().options) {
+      window.alert('You have not configured and imported the Firebase SDK. ' +
+          'Make sure you go through the codelab setup instructions and make ' +
+          'sure you are running the codelab using `firebase serve`');
+    }
+  };
+
+
+
+  window.onload = function() {
+    window.friendlyChat = new socialNet();
+  };
